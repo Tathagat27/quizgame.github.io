85150c30ccae4546233fc7affd08c4cefe0e5fbc
Tathagat27 <tat27302@gmail.com>
2022-01-27 00:05:50 +0530

    fixed

---
+++ public/firestoredb.js
@@ -0,0 +1,84 @@
+const userDetails = document.querySelector('.userDetails')
+const updated = result_box.querySelector(".buttons .Update");
+var x;
+
+function createUserCollection(user){
+    firebase.firestore().collection('users')
+    .doc(user.uid)
+    .set({
+        uid:user.uid,
+        name:user.displayName,
+        points:0,
+    })
+}
+
+async function getuserInfo(userID){
+    if(userID){
+    const userInfoSnap = await firebase.firestore()
+    .collection('users')
+    .doc(userID)
+    .get()
+
+const userInfo = userInfoSnap.data()
+if(userInfo){
+    userDetails.innerHTML = `
+    <h3 style="color:white;"> 👋🏻Hey...${userInfo.name}</h3>
+    <h3 style="color:white;">Points : ${userInfo.points}</h3>
+    `
+}
+
+}else{
+    userDetails.innerHTML = `
+    <h3 style="color:white;">Please Login</h3>`
+}
+}
+
+
+async function getuserInfoRealtime(userID){
+    if(userID){
+    const userdocRef = await firebase.firestore()
+    .collection('users')
+    .doc(userID)
+    userdocRef.onSnapshot((doc)=>{
+         if(doc.exists){
+            const userInfo = doc.data()
+            if(userInfo){
+                userDetails.innerHTML = `
+                <h5 style="color:white;"> Welcome <p style="font-size:35px;font-weight: 800;color:cyan;margin-top:.5rem;">${userInfo.name}</p></h5>
+                <h3 style="color:white;">Your Points : <span style="font-size:35px;font-weight: 800;color:white;margin-top:.5rem;">${userInfo.points}</span></h3>
+                               `
+                x=userInfo.points;
+            }
+            
+            }
+    })
+
+
+}else{
+    userDetails.innerHTML = `
+    <h3 style="color:pink;"> <p style="font-size:35px;font-weight: 500;"></h3>`
+
+}
+
+}
+
+
+
+
+function updateUserProfile(e){
+    e.preventDefault()
+    const userDocRef =  firebase.firestore()
+    .collection('users')
+    .doc(firebase.auth().currentUser.uid)
+
+    
+    
+    userDocRef.update({
+        
+        points:x+score(),
+        
+    })
+
+updated.display = none;
+
+}
--- server.js
+++ server.js
@@ -1,21 +1,46 @@
+const express = require('express');
 const http = require('http');
-const express=require('express');
-const socketio=require('socket.io');
-const app=express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const socketio = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(express.static('${_dirname}/../script'));
 
-const server=http.createServer(app);
-const io=socketio(server);
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection',(sock) => {
-    console.log('someone connected');
-});
+//Start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-server.on('error',(err)=>{
-    console.error(err);
-});
+// Handle a socket connecting request from web client
 
-server.listen(8080,()=>{
-    console.log('server is ready');
+ const connections = [null, null];
+
+io.on('connection', socket => {
+    //console.log('New WS Connection');
+
+    //Find an available player
+    let playerIndex = -1;
+    for (const i in connections){
+        if (connections[i] === null){
+            playerIndex = i;
+            break;
+        }
+    }
+
+ // Tell the connecting client what player number they are
+    socket.emit('player-number', playerIndex)
+
+    console.log(`Player ${playerIndex} has connected`);
+
+    // Ignore player 3
+    if (playerIndex === -1) return;
+
+    connections[playerIndex] = false;
+
+// Tell everyone what player number just connected
+    socket.broadcast.emit('player-connection', playerIndex);
+
 });
